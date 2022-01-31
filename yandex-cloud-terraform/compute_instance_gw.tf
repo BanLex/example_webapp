ebfdbead3ca87515b4aa95ed48bfa03f75f60e8b
BanLex <92094117+BanLex@users.noreply.github.com>
2022-01-31 13:59:40 +0300

    Create compute_instance_gw.tf

---
+++ yandex-cloud-terraform/compute_instance_gw.tf
@@ -0,0 +1,30 @@
+resource "yandex_compute_instance" "gw" {
+  name = "gw"
+  
+  resources {
+    cores  = 2
+    memory = 2
+  }
+
+  boot_disk {
+    initialize_params {
+      image_id = "fd8drj7lsj7btotd7et5"
+      size = "8"
+    }
+  }
+
+  network_interface {
+    subnet_id = yandex_vpc_subnet.subnet-1.id
+    nat       = true
+    ip_address = "192.168.10.3"
+    nat_ip_address = "84.201.129.141"
+    dns_record {
+      fqdn = "gw.ru-central1.internal."
+    }
+  }
+
+  metadata = {
+    user-data = "${file("user")}"
+  }
+
+}
