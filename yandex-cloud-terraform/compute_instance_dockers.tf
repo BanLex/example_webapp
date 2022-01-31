55edbc213e81d1060b47545c9f68a18082ef1268
BanLex <92094117+BanLex@users.noreply.github.com>
2022-01-31 14:15:53 +0300

    Create compute_instance_dockers.tf

---
+++ yandex-cloud-terraform/compute_instance_dockers.tf
@@ -0,0 +1,32 @@
+locals {
+  counts = 4
+}
+
+resource "yandex_compute_instance" "docker" {
+  count = local.counts
+  name = "docker-${count.index}"
+  
+  resources {
+    cores  = 2
+    memory = 2
+  }
+
+  boot_disk {
+    initialize_params {
+      image_id = "fd8f30hur3255mjfi3hq"
+    }
+  }
+
+  network_interface {
+    subnet_id = yandex_vpc_subnet.subnet-1.id
+    nat       = false
+    dns_record {
+      fqdn = "docker-${count.index}.ru-central1.internal."
+    }
+  }
+
+  metadata = {
+    user-data = "${file("user")}"
+  }
+  
+}
