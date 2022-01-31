8a73782ca63459a19394da22479da41224525ff3
BanLex <92094117+BanLex@users.noreply.github.com>
2022-01-31 14:14:44 +0300

    Create compute_instance_nfs.tf

---
+++ yandex-cloud-terraform/compute_instance_nfs.tf
@@ -0,0 +1,29 @@
+resource "yandex_compute_instance" "nfs" {
+  name = "nfs"
+  
+  resources {
+    cores  = 2
+    memory = 2
+  }
+
+  boot_disk {
+    initialize_params {
+      image_id = "fd8f30hur3255mjfi3hq"
+    }
+  }
+
+  network_interface {
+    subnet_id = yandex_vpc_subnet.subnet-1.id
+    nat       = false
+    dns_record {
+      fqdn = "nfs.ru-central1.internal."
+    }
+  }
+
+  metadata = {
+    user-data = "${file("user")}"
+  }
+  
+  
+  
+}
